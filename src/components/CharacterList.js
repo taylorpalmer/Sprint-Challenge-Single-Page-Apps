c69bf5503ce72f25dcbd40f02cf0152edeaa7537
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-03-24 12:26:11 -0700

    working on charList

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -8,21 +10,32 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/"
-      )
-      .then(function(response) {
-        console.log(response.data.results);
-      })
-      .catch(function(error) {
-        console.log("Server Error", error);
-      });
+    const getCharacters = () => {
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/"
+        )
+        .then(function(response) {
+          console.log(response.data.results);
+        })
+        .catch(function(error) {
+          console.log("Server Error", error);
+        });
+    };
+    getCharacters();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>{TODO: `array.map()` over your state here!}</h2>
+      <SearchForm />
+      <h2>
+        {charListState.map(character => {
+          <Link to={`/characters/${character.id}`}>
+            <h2>{character.name}</h2>
+          </Link>;
+        })}
+        ;
+      </h2>
     </section>
   );
 }
