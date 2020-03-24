02f1862dfcae068a7b1189ae2c56d1a286ebb5a7
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-03-24 12:05:21 -0700

    added axios call, useState

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [charListState, setCharListState] = useState({});
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/"
+      )
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }, []);
 
   return (
