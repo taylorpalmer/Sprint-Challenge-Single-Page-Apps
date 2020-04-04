d733a0098eb6ec202d73a15b8e18881fb36ef422
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-04-04 16:25:01 -0700

    charList roughed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList.js";
 
-export default function App = props => {
+const App = props => {
   const [charListState, setCharListState] = useState([]);
+
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const fetchChar = () => {
+      const url = `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.results);
+          setCharListState(data.results);
+        })
+        .catch(function(error) {
+          console.log("Server Error", error);
+        });
+    };
+    fetchChar();
+  }, []);
+  console.log(props);
 
   return (
     <main data-testid="app">
       <Header />
-      <WelcomePage />
+      <Route exact path="/" component={WelcomePage} />
+      <Route
+        path="/charList"
+        render={props => (
+          <CharacterList {...props} charListState={charListState} />
+        )}
+      />
     </main>
   );
-}
+};
+
+export default App;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SearchForm from "./SearchForm";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+const CharacterList = props => {
   // TODO: Add useState to track data from useEffect
-  const [char, setChar] = useState("");
+  const [search, setSearch] = useState("");
+
+  const filtered = props.charListState.filter(char =>
+    char.name.toLowerCase().startsWith(search.toLowerCase())
+  );
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
       <SearchForm onSubmit={searchTerm => setSearch(searchTerm)} />
-      <div></div>
+      <div>
+        {filtered.map(character => (
+          <Link to={`/characters/${character.id}`} key={character.id}>
+            <h2>{character.name}</h2>
+          </Link>
+        ))}
+      </div>
     </section>
   );
-}
+};
+
+export default CharacterList;
